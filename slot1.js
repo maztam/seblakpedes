3b149e669ffbc74d570b776e94caa574f220638f
maztam <maztam@users.noreply.github.com>
2021-11-12 21:24:19 +0700

    Update slot1.js

--- slot1.js
+++ slot1.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://www.trustedgatetocontent.com/eg6cnn35ax?key=4976fb77b439e3d198c9b3c86ffb1bcc");
+      location.replace("");
     },10);
   }
 }, false);
